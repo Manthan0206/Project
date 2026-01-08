0d943bc0708db86bf4426e99551b624fb5c0083b
Manthan0206 <165641908+Manthan0206@users.noreply.github.com>
2026-01-08 20:14:34 +0530

    Add carousel functionality to ranpariyaLab.js

--- RanpariyaLab- Webpage/ranpariyaLab.js
+++ RanpariyaLab- Webpage/ranpariyaLab.js
@@ -26,3 +26,28 @@ displayContent.forEach((button, index) => {
 		}
 	});
 });
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        responsive:{
+            0:{
+                items:2
+            },
+            576:{
+                items:3
+            },
+            768:{
+                items:4
+            },
+            992:{
+                items:5
+            }
+        }
+    });
+});
