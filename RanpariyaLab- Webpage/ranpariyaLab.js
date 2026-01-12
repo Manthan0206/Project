3bf42176c7bba3a90c417ebb50f93a92ce628b42
Manthan0206 <165641908+Manthan0206@users.noreply.github.com>
2026-01-12 20:10:27 +0530

    Refactor header scroll and display toggle functionality

--- RanpariyaLab- Webpage/ranpariyaLab.js
+++ RanpariyaLab- Webpage/ranpariyaLab.js
@@ -1,33 +1,30 @@
-const header = document.getElementById("header");
-
-function headerscroll() {
-	if (window.scrollY >= 20) {
-		header.classList.add("fixed-top");
-	}else{
-		header.classList.remove("fixed-top");
-	}
-}
-
-window.addEventListener("scroll", headerscroll);
-headerscroll();
-
-const displayContent = document.querySelectorAll('.display');
-const spanContent = document.querySelectorAll('.span');
-
-displayContent.forEach((button, index) => {
-	button.addEventListener("click", function() {
-		if (spanContent[index]) {
-			spanContent[index].classList.toggle("d-none");
-			if (spanContent[index].classList.contains("d-none")) {
-				this.textContent = "See more";
-			} else{
-				this.textContent = "See less";
-			}
-		}
-	});
-});
-
 $(document).ready(function(){
+    const header = $("#header");
+    function headerscroll() {
+        if ($(window).scrollTop() >= 1) {
+            header.addClass("fixed-top");
+        }else{
+            header.removeClass("fixed-top")
+        }
+    }
+    $(window).on("scroll", headerscroll);
+    headerscroll();
+
+    $('.display').each(function (index) {
+        $(this).on('click', function () {
+            const $span = $('.span').eq(index);
+            if ($span.length) {
+                $span.toggleClass('d-none');
+                if ($span.hasClass('d-none')) {
+                    $(this).text('See more');
+                } else {
+                    $(this).text('See less');
+                }
+            }
+        });
+    });
+
+
     $(".owl-carousel").owlCarousel({
         loop: true,
         margin: 10,
